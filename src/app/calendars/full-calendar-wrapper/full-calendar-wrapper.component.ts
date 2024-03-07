0db86dc926593a4d8bf9d6ae9d2d58c9f79a190d
marco <mba121@gmail.com>
2024-03-07 15:19:53 +0100

    chore: remove unused code

--- src/app/calendars/full-calendar-wrapper/full-calendar-wrapper.component.ts
+++ src/app/calendars/full-calendar-wrapper/full-calendar-wrapper.component.ts
@@ -18,7 +18,6 @@ import { JsonPipe } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 // import timeGridPlugin from '@fullcalendar/timegrid';
 // import listPlugin from '@fullcalendar/list';
-// import { INITIAL_EVENTS, createEventId } from './event-utils';
 
 @Component({
   selector: 'app-full-calendar-wrapper',
@@ -99,116 +98,42 @@ export class FullCalendarWrapperComponent {
     return opt;
   });
 
-  // calendarOptionsEffect = effect(() => {
-  //   this.calendarOptions.update((opt) => {
-  //     const calendarEvents = this.calendarEventsComputed();
-  //     if (calendarEvents) opt.events = calendarEvents;
-  //     return opt;
-  //   });
+  // calendarOptions = signal<CalendarOptions>({
+  //   plugins: [
+  //     interactionPlugin,
+  //     dayGridPlugin,
+  //     // timeGridPlugin,
+  //     // listPlugin,
+  //   ],
+  //   headerToolbar: {
+  //     left: 'prev,next today',
+  //     center: 'title',
+  //     right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+  //   },
+  //   initialView: 'dayGridMonth',
+  //   // initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+  //   weekends: true,
+  //   editable: true,
+  //   selectable: true,
+  //   selectMirror: true,
+  //   dayMaxEvents: true,
+  //   dateClick: (arg) => this.handleDateClick(arg),
+  //   // eventSources: [this.FAKEcalendarEventsComputed()],
+  //   // select: this.handleDateSelect.bind(this),
+  //   // eventClick: this.handleEventClick.bind(this),
+  //   // eventsSet: this.handleEvents.bind(this)
+  //   /* you can update a remote database when these fire:
+  //   eventAdd:
+  //   eventChange:
+  //   eventRemove:
+  //   */
   // });
-
-  FAKEcalendarEventsComputed = signal<EventSourceInput>(() => {
-    const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
-
-    const result = [
-      {
-        id: 1,
-        title: 'All-day event',
-        start: TODAY_STR,
-      },
-      {
-        id: 2,
-        title: 'Timed event',
-        start: TODAY_STR + 'T00:00:00',
-        end: TODAY_STR + 'T03:00:00',
-      },
-      {
-        id: 3,
-        title: 'Timed event',
-        start: TODAY_STR + 'T12:00:00',
-        end: TODAY_STR + 'T15:00:00',
-      },
-    ];
-    return result;
-  });
-
-  calendarOptions = signal<CalendarOptions>({
-    plugins: [
-      interactionPlugin,
-      dayGridPlugin,
-      // timeGridPlugin,
-      // listPlugin,
-    ],
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
-    },
-    initialView: 'dayGridMonth',
-    // initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true,
-    dateClick: (arg) => this.handleDateClick(arg),
-    // eventSources: [this.FAKEcalendarEventsComputed()],
-    // select: this.handleDateSelect.bind(this),
-    // eventClick: this.handleEventClick.bind(this),
-    // eventsSet: this.handleEvents.bind(this)
-    /* you can update a remote database when these fire:
-    eventAdd:
-    eventChange:
-    eventRemove:
-    */
-  });
 
   handleDateClick(arg: DateClickArg) {
     this.modalMessage = `You clicked on date: ${arg.dateStr}`;
     this.selectedDate.set(arg.date);
     this.isModalVisible.set(true);
   }
-
-  events = signal<CalendarEvent[] | null>([]);
-  // constructor() {
-  //   this.calendarService.getEventsObservable().subscribe((res) => {
-  //     if (res.error) console.error(res.error);
-  //     this.events.set(res.data);
-
-  //     const calendarEvents = res.data?.map((item) => {
-  //       const backgroundColor = this.getShiftColor(item.shift_id);
-  //       const eventItem = {
-  //         title: item.title,
-  //         date: item.start_timestamp,
-  //         backgroundColor: backgroundColor,
-  //         allDay: true,
-  //         display: 'background',
-  //       };
-  //       return eventItem;
-  //     });
-
-  //     this.calendarOptions.update((opt) => {
-  //       opt.events = calendarEvents;
-  //       // opt.events = [
-  //       //   { title: 'event 1', date: '2024-03-01' },
-  //       //   { title: 'event 2', date: '2024-03-02' },
-  //       // ];
-  //       return opt;
-  //     });
-
-  //     // let eventsApi: EventApi[] = [];
-  //     // this.events()?.forEach((item) => {
-  //     //   eventsApi.push({
-  //     //     start: item.start_timestamp,
-  //     //     end: item.end_timestamp,
-  //     //     title: item.title,
-  //     //   });
-  //     // });
-  //     // this.currentEvents.set(eventsApi);
-  //   });
-  // }
-
-  private eventGuid = 0;
 
   isModalVisible = signal(false);
   modalMessage: string = '';
@@ -219,27 +144,23 @@ export class FullCalendarWrapperComponent {
     return currentShift?.color ?? 'blue';
   }
 
-  createEventId() {
-    return String(this.eventGuid++);
-  }
-
   //Thanks to: https://github.com/fullcalendar/fullcalendar-examples/blob/main/angular17/src/app/app.component.ts#L42
-  handleDateSelect(selectInfo: DateSelectArg) {
-    const title = prompt('Please enter a new title for your event');
-    const calendarApi = selectInfo.view.calendar;
+  // handleDateSelect(selectInfo: DateSelectArg) {
+  //   const title = prompt('Please enter a new title for your event');
+  //   const calendarApi = selectInfo.view.calendar;
 
-    calendarApi.unselect(); // clear date selection
+  //   calendarApi.unselect(); // clear date selection
 
-    if (title) {
-      calendarApi.addEvent({
-        id: this.createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
-      });
-    }
-  }
+  //   if (title) {
+  //     calendarApi.addEvent({
+  //       id: this.createEventId(),
+  //       title,
+  //       start: selectInfo.startStr,
+  //       end: selectInfo.endStr,
+  //       allDay: selectInfo.allDay,
+  //     });
+  //   }
+  // }
 
   handleEvents(events: EventApi[]) {
     this.currentEvents.set(events);
